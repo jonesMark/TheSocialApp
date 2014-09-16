ce66d5ca552e2e099ae81655a3cd3feeffecdd18
Lauren Naylor <lauren.naylor@drake.edu>
2014-09-16 01:29:21 -0500

    worked on event calendar - created list of events with a popup description

--- src/com/example/thesocialapp/CalanderofEvents.java
+++ src/com/example/thesocialapp/CalanderofEvents.java
@@ -1,16 +1,56 @@
 package com.example.thesocialapp;
 
-import android.support.v7.app.ActionBarActivity;
+
+
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.PopupWindow;
 
-public class CalanderofEvents extends ActionBarActivity {
+public class CalanderofEvents extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_calanderof_events);
+
+		String[] events = getResources().getStringArray(R.array.friday_event_list);
+		Integer[] icons = {
+				R.drawable.my_calendar,
+				R.drawable.calendar_of_events,
+				R.drawable.featured_events,
+				R.drawable.my_calendar
+		};
+		
+		CustomList adapter = new CustomList(this, events, icons);
+		ListView list = (ListView)findViewById(R.id.list);
+		list.setAdapter(adapter);
+
+		list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				LayoutInflater layoutInflater = (LayoutInflater)getBaseContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+				view = layoutInflater.inflate(R.layout.event_popup, null);
+				final PopupWindow popupWindow = new PopupWindow(view, 750, 1200);
+				popupWindow.showAsDropDown(view, 10, 10);
+
+				Button dismiss = (Button)view.findViewById(R.id.button1);
+				dismiss.setOnClickListener(
+						new View.OnClickListener() {
+							public void onClick(View v) {
+								popupWindow.dismiss();
+							}
+
+
+						});
+			}
+		});
 	}
 
 	@Override
--- src/com/example/thesocialapp/FeaturedEvents.java
+++ src/com/example/thesocialapp/FeaturedEvents.java
@@ -1,11 +1,12 @@
 package com.example.thesocialapp;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class FeaturedEvents extends ActionBarActivity {
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
