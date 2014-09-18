d082f598e17a4a861abd17aa8d08e22dd960122f
unknown <christopher.siegel@drake.edu>
2014-09-17 21:58:02 -0500

    a

--- src/com/example/thesocialapp/FeaturedEvents.java
+++ src/com/example/thesocialapp/FeaturedEvents.java
@@ -8,14 +8,36 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 public class FeaturedEvents extends Fragment  {
 		public View onCreateView(LayoutInflater inflater, ViewGroup container, 
 				Bundle savedInstanceState){
 			View view = inflater.inflate(R.layout.activity_featured_events, container, false);
-			//TextView textview = (TextView) view.findViewById(R.id.tabtextview);
-			//textview.setText("hello");
+			
+			ImageButton imagebuttonfeatured = (ImageButton) view.findViewById(R.id.imagebuttonFeatured);
+	        imagebuttonfeatured.setOnClickListener(
+	        		new OnClickListener() {
+	        		public void onClick(View v){
+	        			LayoutInflater layoutInflater = (LayoutInflater)getActivity().getLayoutInflater();
+	        			View view = layoutInflater.inflate(R.layout.event_popup, null);
+	        			final PopupWindow popupWindow = new PopupWindow(view, 750, 1200);
+	        			popupWindow.showAsDropDown(view, 10, 10);
+
+	        			Button dismiss = (Button)view.findViewById(R.id.button1);
+	        			dismiss.setOnClickListener(
+	        					new View.OnClickListener() {
+	        						public void onClick(View v) {
+	        							popupWindow.dismiss();
+	        						}
+	        		});
+	        		}
+	        		}
+	        		);
 			return view;
 		}
 
